462bf6e31bb68553cfb05ac5a668cbbb9ea3fa81
Pradipta Samanta <k202170@levante6.lvt.dkrz.de>
2023-03-23 15:17:32 +0100

    Adeed the initial files: CMakeLists.txt src/lulesh.cc src/lulesh.h src/mesh.h src/utils.cc

---
+++ src/lulesh.h
@@ -0,0 +1,104 @@
+#include <math.h>
+//#define LULESH_SHOW_PROGRESS 1
+
+enum { VolumeError = -1, QStopError = -2 } ;
+
+/****************************************************/
+/* Allow flexibility for arithmetic representations */
+/****************************************************/
+
+/* Could also support fixed point and interval arithmetic types */
+typedef float        real4 ;
+typedef double       real8 ;
+typedef long double  real10 ;  /* 10 bytes on x86 */
+
+typedef int    Index_t ; /* array subscript and loop index */
+typedef real8  Real_t ;  /* floating point representation */
+typedef int    Int_t ;   /* integer representation */
+
+inline real4  SQRT(real4  arg) { return sqrtf(arg) ; }
+inline real8  SQRT(real8  arg) { return sqrt(arg) ; }
+inline real10 SQRT(real10 arg) { return sqrtl(arg) ; }
+
+inline real4  CBRT(real4  arg) { return cbrtf(arg) ; }
+inline real8  CBRT(real8  arg) { return cbrt(arg) ; }
+inline real10 CBRT(real10 arg) { return cbrtl(arg) ; }
+
+inline real4  FABS(real4  arg) { return fabsf(arg) ; }
+inline real8  FABS(real8  arg) { return fabs(arg) ; }
+inline real10 FABS(real10 arg) { return fabsl(arg) ; }
+
+
+/************************************************************/
+/* Allow for flexible data layout experiments by separating */
+/* array interface from underlying implementation.          */
+/************************************************************/
+
+
+template <typename T>
+T *Allocate(size_t size)
+{
+   return static_cast<T *>(malloc(sizeof(T)*size)) ;
+}
+
+template <typename T>
+void Release(T **ptr)
+{
+   if (*ptr != NULL) {
+      free(*ptr) ;
+      *ptr = NULL ;
+   }
+}
+
+
+/* Stuff needed for boundary conditions */
+/* 2 BCs on each of 6 hexahedral faces (12 bits) */
+#define XI_M        0x003
+#define XI_M_SYMM   0x001
+#define XI_M_FREE   0x002
+
+#define XI_P        0x00c
+#define XI_P_SYMM   0x004
+#define XI_P_FREE   0x008
+
+#define ETA_M       0x030
+#define ETA_M_SYMM  0x010
+#define ETA_M_FREE  0x020
+
+#define ETA_P       0x0c0
+#define ETA_P_SYMM  0x040
+#define ETA_P_FREE  0x080
+
+#define ZETA_M      0x300
+#define ZETA_M_SYMM 0x100
+#define ZETA_M_FREE 0x200
+
+#define ZETA_P      0xc00
+#define ZETA_P_SYMM 0x400
+#define ZETA_P_FREE 0x800
+
+
+//static inline
+//void TimeIncrement(void);
+
+//static inline
+//void LagrangeLeapFrog(void);
+
+Real_t CalcElemVolume( const Real_t x[8], const Real_t y[8], const Real_t z[8] );
+
+Real_t CalcElemCharacteristicLength( const Real_t x[8],
+                                     const Real_t y[8],
+                                     const Real_t z[8],
+                                     const Real_t volume);
+
+void CalcElemShapeFunctionDerivatives( const Real_t* const x,
+                                       const Real_t* const y,
+                                       const Real_t* const z,
+                                       Real_t b[][8],
+                                       Real_t* const volume );
+
+// calc_force.cc
+void CalcVolumeForceForElems(void);
+
+void InitStressTermsForElems(Index_t numElem, 
+                             Real_t *sigxx, Real_t *sigyy, Real_t *sigzz);
